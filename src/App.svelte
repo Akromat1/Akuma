7f700b2b88c42e2dc376e661c239848801652723
Акромат <108363130+Akromat1@users.noreply.github.com>
2023-09-18 00:07:43 +0300

    Кнопки

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Button from "./lib/Button.svelte";
+  import Icon from "./lib/Icon.svelte";
+</script>
+<Button>
+<Icon/>Play
+</Button>
+<Button rounded={false}>
+
+</Button>
+<Button>
+  
+</Button>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,34 @@
+<script>
+export let rounded=true
+</script>
+<button
+class:active={rounded}>
+    
+    <slot>
+       dscazcd 
+    </slot>
+</button>
+<style>
+    .active{
+       border-radius: 50% 50%;
+    }
+button {
+    padding: 14px 20px;
+    border-radius: 12px;
+    background-color:#007AFF ;
+    color: white;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    column-gap: 4px;
+}
+button:hover {
+    box-shadow: 0 0 0 99999px inset rgba(0, 0, 0, 0.1);
+  }
+
+  button:active {
+    box-shadow: 0 0 0 99999px inset rgba(0, 0, 0, 0.2);
+  }
+
+</style>
